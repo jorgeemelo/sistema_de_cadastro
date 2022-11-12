1a2fdf92b5c697e0e3c9c6033973850248059000
Jorge Melo <jorgeemelo7@gmail.com>
2022-11-12 20:01:38 -0300

    update

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from tkinter import messagebox
 from tkinter import filedialog as fd
 from PIL import Image, ImageTk
 from tkcalendar import*
-
+import sys
 from view import atualizar_form, deletar_form, inserir_form, ver_form
+
+sys.setrecursionlimit(10000)
 
 ################# cores ###############
 co0 = "#2e2d2b"  # Preta
@@ -85,7 +87,6 @@ def inserir():
     e_cal.delete(0, 'end')
     e_nome_pai.delete(0, 'end')
     e_nome_mae.delete(0, 'end')
-    #e_casados.delete(0, 'end') TESTING
     e_padrinho.delete(0, 'end')
     e_madrinha.delete(0, 'end')
 
@@ -102,13 +103,13 @@ def atualizar():
         treev_dicionario = tree.item(treev_dados)
         treev_lista = treev_dicionario['values']
 
-        padrinho = treev_lista[0]
+        valor = treev_lista[0]
 
         e_nome.delete(0, 'end')
         e_cal.delete(0, 'end')
         e_nome_pai.delete(0, 'end')
         e_nome_mae.delete(0, 'end')
-        e_casados.delete(0, 'end')
+        #e_casados.delete(0, 'end')
         e_padrinho.delete(0, 'end')
         e_madrinha.delete(0, 'end')
 
@@ -124,8 +125,6 @@ def atualizar():
 
         def update():
 
-            global imagem
-
             nome = e_nome.get()
             data_de_nasc = e_cal.get()
             nome_pai = e_nome_pai.get()
@@ -133,9 +132,6 @@ def atualizar():
             casados = e_casados.get()
             padrinho = e_padrinho.get()
             madrinha = e_madrinha.get()
-
-            if imagem == '':
-                imagem = e_madrinha.insert(0, treev_lista[7])
 
             lista_atualizar = [nome, nome_pai, nome_mae, casados, data_de_nasc, padrinho, madrinha, id]
 
@@ -165,9 +161,7 @@ def atualizar():
             mostrar()
 
         botao_confirmar = Button(frameMeio, command=update, text="Confirmar".upper(), width=13, height=1, bg=co2, fg=co1,font=('ivy 8 bold'),relief=RAISED, overrelief=RIDGE)
-
         botao_confirmar.place(x=330, y=185)
-
 
     except IndexError:
         messagebox.showerror('Erro', 'Seleciona um dos dados na tabela')
@@ -194,71 +188,52 @@ def deletar():
         messagebox.showerror('Erro', 'Seleciona um dos dados na tabela')
 
 # criando entradas
-l_nome = Label(frameMeio, text="Nome", height=1,anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_nome = Label(frameMeio, text="Nome do Batizado", height=1,anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
 l_nome.place(x=20, y=10)
-
-e_nome = Entry(frameMeio, width=30, justify='left',relief="solid")
-e_nome.place(x=130, y=11)
+e_nome = Entry(frameMeio, width=40, justify='left',relief="solid")
+e_nome.place(x=150, y=11)
 
 # ADICIONAR OUTRA VARIAVEL DE DATA PARA 'DIA_BATISMO'
 l_cal = Label(frameMeio, text="Data de Nascimento", height=1,anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
-l_cal.place(x=20, y=130)
-
+l_cal.place(x=20, y=40)
 e_cal = DateEntry(frameMeio, width=12, background='darkblue', foreground='white', borderwidth=2, year=2022)
-e_cal.place(x=130, y=131)
+e_cal.place(x=160, y=41)
 
 l_nome_pai = Label(frameMeio, text="Nome do Pai", height=1,anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
-l_nome_pai.place(x=20, y=40)
-
-e_nome_pai = Entry(frameMeio, width=30, justify='left',relief="solid")
-e_nome_pai.place(x=130, y=41)
+l_nome_pai.place(x=20, y=130)
+e_nome_pai = Entry(frameMeio, width=40, justify='left',relief="solid")
+e_nome_pai.place(x=150, y=131)
 
 l_nome_mae = Label(frameMeio, text="Nome da Mãe", height=1,anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
 l_nome_mae.place(x=20, y=70)
-e_nome_mae = Entry(frameMeio, width=30, justify='left',relief="solid")
-e_nome_mae.place(x=130, y=71)
-
-
-
-#v_casados = StringVar()
-# v_casados = ttk.Combobox(janela, textvariable=d_casados)
-# e_casados['values'] = ['Sim', 'Não']
-# e_casados['state'] = 'readonly'
-# v_casados.place(x=130, y=101)
-#e_casados.bind('<<ComboboxSelected>>', v_casados.get())
-
-
+e_nome_mae = Entry(frameMeio, width=40, justify='left',relief="solid")
+e_nome_mae.place(x=150, y=71)
 
 l_casados = Label(frameMeio, text="Casados na Igreja", height=1,anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
 l_casados.place(x=20, y=100)
-
 e_casados = StringVar('')
 cb_casados = ttk.Combobox(frameMeio, width=10, textvariable=e_casados)
 cb_casados['values'] = ['Sim', 'Não']
 cb_casados.bind('<<ComboboxSelected>>', e_casados.get())
 cb_casados['state'] = 'readonly'
-cb_casados.place(x=130, y=101)
-
-
+cb_casados.place(x=150, y=101)
 
 l_padrinho = Label(frameMeio, text="Nome do Padrinho", height=1,anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
 l_padrinho.place(x=20, y=160)
-
-e_padrinho = Entry(frameMeio, width=30, justify='left',relief="solid")
-e_padrinho.place(x=130, y=161)
+e_padrinho = Entry(frameMeio, width=40, justify='left',relief="solid")
+e_padrinho.place(x=150, y=161)
 
 l_madrinha = Label(frameMeio, text="Nome da Madrinha", height=1,anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
 l_madrinha.place(x=20, y=190)
+e_madrinha = Entry(frameMeio, width=40, justify='left',relief="solid")
+e_madrinha.place(x=150, y=191)
 
-e_madrinha = Entry(frameMeio, width=30, justify='left',relief="solid")
-e_madrinha.place(x=130, y=191)
-
-l_carregar = Label(frameMeio, text="TEXTO", height=1,anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
-l_carregar.place(x=20, y=220)
+#l_carregar = Label(frameMeio, text="TEXTO", height=1,anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+#l_carregar.place(x=20, y=220)
 
 # Botao Carregar
-botao_carregar = Button(frameMeio, compound=CENTER, anchor=CENTER, text="carregar".upper(), width=30, overrelief=RIDGE,  font=('ivy 8'),bg=co1, fg=co0 )
-botao_carregar.place(x=130, y=221)
+#botao_carregar = Button(frameMeio, compound=CENTER, anchor=CENTER, text="carregar".upper(), width=30, overrelief=RIDGE,  font=('ivy 8'),bg=co1, fg=co0 )
+#botao_carregar.place(x=130, y=221)
 
 # Botao Inserir
 img_add = Image.open('main_files\icon\icon_add.png')
@@ -291,21 +266,6 @@ img_item = ImageTk.PhotoImage(img_item)
 
 botao_ver = Button(frameBotoes, image=img_item, compound=LEFT, anchor=NW, text="   Ver item".upper(), width=150, overrelief=RIDGE,  font=('ivy 10'),bg=co1, fg=co0)
 botao_ver.place(x=560, y=11)
-
-'''
-# Labels Quantidade total e padrinhoes
-l_total = Label(frameMeio, width=14, height=2,anchor=CENTER, font=('Ivy 17 bold'), bg=co7, fg=co1, relief=FLAT)
-l_total.place(x=450, y=17)
-
-l_padrinho_total = Label(frameMeio, text='  padrinho Total de todos os itens  ' ,anchor=NW, font=('Ivy 10 bold'), bg=co7, fg=co1)
-l_padrinho_total.place(x=450, y=12)
-
-l_qtd = Label(frameMeio, width=10, height=2,anchor=CENTER, font=('Ivy 25 bold'), bg=co7, fg=co1, relief=FLAT)
-l_qtd.place(x=450, y=90)
-
-l_qtd_itens = Label(frameMeio, text='Quantidade total de itens' ,anchor=NW, font=('Ivy 10 bold'), bg=co7, fg=co1)
-l_qtd_itens.place(x=460, y=92)
-'''
 
 # funcao para mostrar
 def mostrar():
@@ -348,13 +308,6 @@ def mostrar():
 
     for iten in lista_itens:
         quantidade.append(iten[6])
-
-
-#    Total_padrinho = sum(quantidade)
-#    Total_itens = len(quantidade)
-#
-#    l_total['text'] = 'R$ {:,.2f}'.format(Total_padrinho)
-#    l_qtd['text'] = Total_itens
 
 mostrar()
 
